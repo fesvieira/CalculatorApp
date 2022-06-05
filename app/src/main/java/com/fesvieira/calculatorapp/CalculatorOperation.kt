136cc2f1ae9199545db379a52e4e2c60e90a8f13
fesvieira <felipe2204sv@gmail.com>
2022-06-05 11:52:35 -0300

    finish calculator

--- app/src/main/java/com/fesvieira/calculatorapp/CalculatorOperation.kt
+++ app/src/main/java/com/fesvieira/calculatorapp/CalculatorOperation.kt
@@ -1,6 +1,6 @@
 package com.fesvieira.calculatorapp
 
-sealed class CalculatorOperation(val symbol:String) {
+sealed class CalculatorOperation(val symbol: String) {
     object Add: CalculatorOperation("+")
     object Subtract: CalculatorOperation("-")
     object Multiply: CalculatorOperation("x")
